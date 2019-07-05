f52c54379978567cc448ab0346a859b49d7d0199
Adriano Sokax <asosam91@gmail.com>
2019-07-05 11:49:37 -0500

    recursos path was added

---
+++ recursos/script.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+
+comando = "pip install pandas"
+os.system(comando)
+
+import pandas as pd 
+
+archivo = "UniversityData.csv"
+df = pd.read_csv(archivo, usecols=["University",  "lat", "lon"], dtype={'University': 'str',  'lat': 'float64', 'lon': 'float64'})
+df = df.dropna()
+df.to_csv(archivo) 
